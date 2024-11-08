8dd93090748a343d80e119af108be0c88199359b
Shahrik Amin <shahrikamin2001@gmail.com>
2024-11-08 10:57:00 -0500

    Restructure project and update .gitignore to exclude target folder

---
+++ src/main/java/com/group35/project/Main.java
@@ -0,0 +1,9 @@
+package com.group35.project;
+
+public class Main {
+
+    public static void main(String[] args) {  // Correct main method signature
+        System.out.println("hello world");
+    }
+
+}
