3eb4c73b1d5006b3e029d10a3f4023a39aeb4f69
Shahrik Amin <shahrikamin2001@gmail.com>
2024-11-08 10:48:40 -0500

    Remove nested folder structure

--- SYSC4806-Group-35/src/main/java/com/group35/project/Main.java
+++
@@ -1,9 +0,0 @@
-package com.group35.project;
-
-public class Main {
-
-    public static void main(String[] args) {  // Correct main method signature
-        System.out.println("hello world");
-    }
-
-}
