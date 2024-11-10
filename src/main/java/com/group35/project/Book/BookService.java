d96ec4120d780e8e1aa224eccdb1f9b0a1167eff
chibuzo <chibuzookpara@cmail.carleton.ca>
2024-11-09 20:32:55 -0500

    First commit of book feature. Contains model, service and controller classes including CrudRepository interface.

---
+++ src/main/java/com/group35/project/Book/BookService.java
@@ -0,0 +1,51 @@
+package com.group35.project.Book;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+/***
+ * BookService class for logic and book operations.
+ * @author Chibuzo Okpara
+ */
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    //Create a new book
+    public Book createBook(Book book){
+        return this.bookRepository.save(book);
+    }
+
+    //Find a book by ID
+    public Optional<Book> findBookById(Long id) {
+        return this.bookRepository.findById(id);
+    }
+
+    // Find Books by Title
+    public List<Book> findBooksByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+    //Find books by a word in the title
+    public List<Book> findBooksByTitleContaining(String keyword) {
+        return bookRepository.findByTitleContaining(keyword);
+    }
+
+    //Return all books
+    public Iterable<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Delete a Book by ID
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
